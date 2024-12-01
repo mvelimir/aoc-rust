130c5676a8e889c1a847572a4fb96a7d687709d0
mvelimir <velimirmilinkovic1@gmail.com>
2024-12-01 13:10:00 +0100

    Complete day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,14 +1,9 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (mut first_vec, mut second_vec): (Vec<_>, Vec<_>) = input
-        .lines()
-        .map(|x| {
-            let mut number_it = x.split_whitespace().map(|x| x.parse::<u32>().unwrap());
-
-            (number_it.next().unwrap(), number_it.next().unwrap())
-        })
-        .unzip();
+    let (mut first_vec, mut second_vec) = parse_input(input);
 
     first_vec.sort();
     second_vec.sort();
@@ -23,7 +18,31 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (first_vec, second_vec) = parse_input(input);
+
+    let mut hash_map = HashMap::new();
+
+    for el in second_vec {
+        *hash_map.entry(el).or_insert(0) += 1;
+    }
+
+    let similarity = first_vec
+        .iter()
+        .map(|x| x * hash_map.get(x).unwrap_or(&0))
+        .sum();
+
+    Some(similarity)
+}
+
+fn parse_input(input: &str) -> (Vec<u32>, Vec<u32>) {
+    input
+        .lines()
+        .map(|x| {
+            let mut number_it = x.split_whitespace().map(|x| x.parse::<u32>().unwrap());
+
+            (number_it.next().unwrap(), number_it.next().unwrap())
+        })
+        .unzip()
 }
 
 #[cfg(test)]
@@ -39,6 +58,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(31));
     }
 }
