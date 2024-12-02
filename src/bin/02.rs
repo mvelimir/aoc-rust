46f04766ab07b58b4f74c850cf720752f55ec10e
mvelimir <velimirmilinkovic1@gmail.com>
2024-12-02 08:25:53 +0100

    Complete day 2 part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -9,15 +9,7 @@ pub fn part_one(input: &str) -> Option<u32> {
                 .map(|x| x.parse::<i32>().unwrap())
                 .collect();
 
-            let positive = number_vec[1] - number_vec[0] > 0;
-
-            number_vec
-                .windows(2)
-                .map(|x| x[1] - x[0])
-                .filter(|x| x.abs() >= 1 && x.abs() <= 3)
-                .filter(|x| if positive { *x > 0 } else { *x <= 0 })
-                .count()
-                == number_vec.len() - 1
+            check_report(number_vec)
         })
         .filter(|x| *x == true)
         .count() as u32;
@@ -26,7 +18,41 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let count = input
+        .lines()
+        .map(|x| {
+            let number_vec: Vec<_> = x
+                .split_whitespace()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect();
+
+            let vec_iter = std::iter::once(number_vec.clone()).chain(
+                (0..number_vec.len()).into_iter().map(|i| {
+                    let mut vec = number_vec.clone();
+
+                    vec.remove(i);
+
+                    vec
+                }),
+            );
+
+            vec_iter.map(check_report).find(|x| *x).is_some()
+        })
+        .filter(|x| *x == true)
+        .count() as u32;
+
+    Some(count)
+}
+
+fn check_report(vec: Vec<i32>) -> bool {
+    let positive = vec[1] - vec[0] > 0;
+
+    vec.windows(2)
+        .map(|x| x[1] - x[0])
+        .filter(|x| x.abs() >= 1 && x.abs() <= 3)
+        .filter(|x| if positive { *x > 0 } else { *x <= 0 })
+        .count()
+        == vec.len() - 1
 }
 
 #[cfg(test)]
@@ -42,6 +68,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
