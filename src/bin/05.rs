d219cc5553c414434a2acefbdb4d8093702971b5
mvelimir <velimirmilinkovic1@gmail.com>
2024-12-05 17:23:00 +0100

    Complete day 5 part 2

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,8 +1,49 @@
-use std::collections::{HashMap, HashSet};
+use std::{cmp::Ordering, collections::HashSet};
 
 advent_of_code::solution!(5);
 
 pub fn part_one(input: &str) -> Option<u32> {
+    let mut result = 0;
+
+    calculate_result(input, |update, less_than_set| {
+        if update
+            .windows(2)
+            .all(|w| less_than_set.contains(&(w[0], w[1])))
+        {
+            result += update[update.len() / 2];
+        }
+    });
+
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut result = 0;
+
+    calculate_result(input, |update, less_than_set| {
+        if !update
+            .windows(2)
+            .all(|w| less_than_set.contains(&(w[0], w[1])))
+        {
+            update.sort_by(|a, b| {
+                if less_than_set.contains(&(*a, *b)) {
+                    Ordering::Less
+                } else {
+                    Ordering::Greater
+                }
+            });
+
+            result += update[update.len() / 2];
+        }
+    });
+
+    Some(result)
+}
+
+fn calculate_result<F>(input: &str, mut calculation: F) -> Option<u32>
+where
+    F: FnMut(&mut [u32], &HashSet<(u32, u32)>),
+{
     let [ordering_rules_str, updates_str] = input.split("\n\n").collect::<Vec<_>>()[..] else {
         panic!();
     };
@@ -23,10 +64,12 @@ pub fn part_one(input: &str) -> Option<u32> {
     }
 
     for line in updates_str.lines() {
-        let update = line
+        let mut update = line
             .split(",")
             .map(|x| x.parse().unwrap())
             .collect::<Vec<u32>>();
+
+        calculation(&mut update, &less_than_set);
 
         if update
             .windows(2)
@@ -37,10 +80,6 @@ pub fn part_one(input: &str) -> Option<u32> {
     }
 
     Some(result)
-}
-
-pub fn part_two(input: &str) -> Option<u32> {
-    None
 }
 
 #[cfg(test)]
@@ -56,6 +95,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(123));
     }
 }
