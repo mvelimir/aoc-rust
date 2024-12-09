794a9d9abd1a36d49bce1c9e94f88b27b2c3fcf5
mvelimir <velimirmilinkovic1@gmail.com>
2024-12-09 20:05:44 +0100

    Solve day 9 part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,9 +1,17 @@
+use std::io::empty;
+
 use advent_of_code::template::aoc_cli::check;
 
 advent_of_code::solution!(9);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let mut vec: Vec<u64> = input.lines().next().unwrap().chars().map(|x| x.to_digit(10).unwrap() as u64).collect();
+    let mut vec: Vec<u64> = input
+        .lines()
+        .next()
+        .unwrap()
+        .chars()
+        .map(|x| x.to_digit(10).unwrap() as u64)
+        .collect();
     let mut checksum = 0;
     let last_idx = vec.len() - 1;
 
@@ -46,12 +54,65 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(checksum)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut vec: Vec<u64> = input
+        .lines()
+        .next()
+        .unwrap()
+        .chars()
+        .map(|x| x.to_digit(10).unwrap() as u64)
+        .collect();
+    let mut checksum = 0;
+    let last_idx = vec.len() - 1;
+
+    let mut moved_vec = vec![false; vec.len() / 2 + 1];
+    let mut space_vec: Vec<_> = vec
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| i % 2 == 1)
+        .map(|(_, &val)| val)
+        .collect();
+
+    for end_idx in (2..=last_idx - last_idx % 2).rev().step_by(2) {
+        let mut empty_idx = 1;
+        let mut curr_pos = vec[0];
+
+        while empty_idx < end_idx {
+            if vec[end_idx] <= space_vec[empty_idx / 2] {
+                curr_pos += vec[empty_idx] - space_vec[empty_idx / 2];
+                checksum += end_idx as u64 / 2 * sum(curr_pos, curr_pos + vec[end_idx]);
+                space_vec[empty_idx / 2] -= vec[end_idx];
+                moved_vec[end_idx / 2] = true;
+
+                break;
+            }
+
+            curr_pos += vec[empty_idx] + vec[empty_idx + 1];
+            empty_idx += 2;
+        }
+    }
+
+    let mut start_idx = 0;
+    let mut curr_pos = 0;
+
+    loop {
+        if moved_vec[start_idx / 2] != true {
+            checksum += start_idx as u64 / 2 * sum(curr_pos, curr_pos + vec[start_idx]);
+        }
+
+        start_idx += 2;
+        if start_idx >= vec.len() {
+            break;
+        }
+
+        curr_pos += vec[start_idx - 2] + vec[start_idx - 1];
+    }
+
+    Some(checksum)
 }
 
 fn sum(from: u64, to: u64) -> u64 {
-    to * (to - 1) / 2 + from * (from - 1) / 2
+    (from..to).sum()
 }
 
 #[cfg(test)]
@@ -67,6 +128,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2858));
     }
 }
