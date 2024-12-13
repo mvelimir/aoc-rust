7cfa420d41b165b7d5a21c4884aac9fd92e6bef0
mvelimir <velimirmilinkovic1@gmail.com>
2024-12-13 12:01:21 +0100

    Solve day 13 part 2

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,6 +1,6 @@
 advent_of_code::solution!(13);
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     let sum = input
         .split("\n\n")
         .map(|str| {
@@ -18,11 +18,30 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(sum)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let sum = input
+        .split("\n\n")
+        .map(|str| {
+            let nums = parse_numbers(str);
+            if let Some((a, b)) = solve_linear_system(
+                nums[0],
+                nums[2],
+                nums[4] + 10000000000000,
+                nums[1],
+                nums[3],
+                nums[5] + 10000000000000,
+            ) {
+                a * 3 + b
+            } else {
+                0
+            }
+        })
+        .sum();
+
+    Some(sum)
 }
 
-fn parse_numbers(input: &str) -> Vec<u32> {
+fn parse_numbers(input: &str) -> Vec<u64> {
     let mut numbers = Vec::new();
     let mut curr_number = String::new();
 
@@ -30,7 +49,7 @@ fn parse_numbers(input: &str) -> Vec<u32> {
         if ch.is_digit(10) {
             curr_number.push(ch);
         } else if !curr_number.is_empty() {
-            if let Ok(num) = curr_number.parse::<u32>() {
+            if let Ok(num) = curr_number.parse::<u64>() {
                 numbers.push(num);
             }
             curr_number.clear();
@@ -38,7 +57,7 @@ fn parse_numbers(input: &str) -> Vec<u32> {
     }
 
     if !curr_number.is_empty() {
-        if let Ok(num) = curr_number.parse::<u32>() {
+        if let Ok(num) = curr_number.parse::<u64>() {
             numbers.push(num);
         }
     }
@@ -46,13 +65,13 @@ fn parse_numbers(input: &str) -> Vec<u32> {
     numbers
 }
 
-fn solve_linear_system(a1: u32, b1: u32, c1: u32, a2: u32, b2: u32, c2: u32) -> Option<(u32, u32)> {
-    let a1 = a1 as i32;
-    let b1 = b1 as i32;
-    let c1 = c1 as i32;
-    let a2 = a2 as i32;
-    let b2 = b2 as i32;
-    let c2 = c2 as i32;
+fn solve_linear_system(a1: u64, b1: u64, c1: u64, a2: u64, b2: u64, c2: u64) -> Option<(u64, u64)> {
+    let a1 = a1 as i64;
+    let b1 = b1 as i64;
+    let c1 = c1 as i64;
+    let a2 = a2 as i64;
+    let b2 = b2 as i64;
+    let c2 = c2 as i64;
 
     let det = a1 * b2 - a2 * b1;
 
@@ -70,7 +89,7 @@ fn solve_linear_system(a1: u32, b1: u32, c1: u32, a2: u32, b2: u32, c2: u32) -> 
     let x = x_num / det;
     let y = y_num / det;
 
-    Some((x as u32, y as u32))
+    Some((x as u64, y as u64))
 }
 #[cfg(test)]
 mod tests {
@@ -85,6 +104,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(875318608908));
     }
 }
