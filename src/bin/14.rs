4777d3e500c4514ae69592823a9c91377a5a940c
mvelimir <velimirmilinkovic1@gmail.com>
2024-12-14 18:27:23 +0100

    Solve day 14 part 2

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,6 +1,50 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 
 advent_of_code::solution!(14);
+
+struct Robot {
+    pos: (i32, i32),
+    vel: (i32, i32),
+}
+
+struct Grid {
+    data: Vec<char>,
+    width: usize,
+    height: usize,
+}
+
+impl Grid {
+    fn new(dims: (usize, usize)) -> Self {
+        let width = dims.0;
+        let height = dims.1;
+
+        Grid {
+            data: vec![' '; width * height],
+            width,
+            height,
+        }
+    }
+
+    fn at_mut(&mut self, pos: &(usize, usize)) -> &mut char {
+        let idx = self.width * pos.1 + pos.0;
+        &mut self.data[idx]
+    }
+
+    fn to_str(&self) -> String {
+        self.data
+            .chunks(self.width)
+            .map(|chunk| chunk.iter().collect::<String>())
+            .collect::<Vec<_>>()
+            .join("\n")
+    }
+
+    fn has_horizontal_line(&self) -> bool {
+        self.data
+            .chunks(self.width)
+            .map(|chunk| chunk.iter().collect::<String>())
+            .any(|line| line.contains(&"*".repeat(30)))
+    }
+}
 
 pub fn part_one(input: &str) -> Option<u32> {
     let dims = (101, 103);
@@ -28,7 +72,51 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
+    let dims = (101, 103);
+
+    let mut robots = Vec::new();
+
+    for line in input.lines() {
+        let nums: Vec<i32> = line
+            .split(|c: char| !c.is_digit(10) && c != '-')
+            .filter_map(|s| s.parse().ok())
+            .collect();
+
+        let pos = (nums[0], nums[1]);
+        let vel = (nums[2], nums[3]);
+
+        robots.push(Robot { pos, vel });
+    }
+
+    for second in 1..1000001 {
+        for i in 0..robots.len() {
+            robots[i].pos = calculate_board_position(dims, robots[i].pos, robots[i].vel, 1);
+        }
+
+        if print_grid_with_robots(dims, &robots, second) {
+            break;
+        };
+    }
+
     None
+}
+
+fn print_grid_with_robots(dims: (i32, i32), robots: &[Robot], second: u32) -> bool {
+    let mut grid = Grid::new((dims.0 as usize, dims.1 as usize));
+
+    for robot in robots {
+        *grid.at_mut(&(robot.pos.0 as usize, robot.pos.1 as usize)) = '*';
+    }
+
+    if grid.has_horizontal_line() {
+        println!("After {} seconds:", second);
+        println!("{}", grid.to_str());
+        println!("");
+
+        return true;
+    }
+
+    false
 }
 
 fn calculate_board_position(
