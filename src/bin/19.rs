b8ef309d3468f0cc80227e139f35c6c4740fe2d3
mvelimir <velimirmilinkovic1@gmail.com>
2024-12-19 22:16:54 +0100

    Solve day 19 part 1

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -1,11 +1,54 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(19);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let (towels, patterns) = input.split_once("\n\n").unwrap();
+
+    let towels = towels.split(", ").collect::<Vec<_>>();
+
+    let mut pattern_possibility = HashMap::new();
+
+    let res = patterns
+        .lines()
+        .filter(|pattern| is_pattern_possible(pattern, &towels, &mut pattern_possibility))
+        .count();
+
+    Some(res as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     None
+}
+
+fn is_pattern_possible<'a>(
+    pattern: &'a str,
+    towels: &[&str],
+    pattern_possibility: &mut HashMap<String, bool>,
+) -> bool {
+    if pattern.is_empty() {
+        return true;
+    }
+
+    if let Some(res) = pattern_possibility.get(pattern) {
+        return *res;
+    }
+
+    for i in 1..pattern.len() + 1 {
+        for &towel in towels.iter() {
+            if pattern[0..i] == *towel {
+                if is_pattern_possible(&pattern[i..], towels, pattern_possibility) {
+                    pattern_possibility.entry(String::from(pattern)).or_insert(true);
+
+                    return true;
+                } else {
+                    pattern_possibility.entry(String::from(&pattern[i..])).or_insert(false);
+                }
+            }
+        }
+    }
+
+    return false;
 }
 
 #[cfg(test)]
@@ -15,7 +58,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 
     #[test]
